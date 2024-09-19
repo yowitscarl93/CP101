b1eb89e7ac27a8065fa1d9bb3874ebcf376dbe1f
Meow PRIMA <migzkieprims@gmail.com>
2024-09-19 18:57:05 +0700

    Create Prima,Miguel.py

---
+++ Activity 2/Prima,Miguel.py
@@ -0,0 +1,8 @@
+sub_math = float(input("grade in math:"))
+sub_science = float(input("grade in science:"))
+sub_english = float(input("grade in english:"))
+
+add_all_subject = sub_math + sub_science + sub_english
+
+total_average = (add_all_subject/3)
+print(total_average)
