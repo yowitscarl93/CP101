e0400291d30887f558b490c6ba45a1e1a90ee368
Ricky B. Almirol <darkmoon9175@gmail.com>
2024-09-19 04:54:45 +0100

    Create Almirol.py
    Activity 1

---
+++ Activity 1/Almirol.py
@@ -0,0 +1,11 @@
+# Find the value of variable x.
+x = 1 + 2 * 3 - 4 / 5
+print (x)
+
+# Find the value of variable y.
+y = 1 * x / 4 ** 2
+print (y)
+
+# Find the value of variable z.
+z = x + y
+print (z)
