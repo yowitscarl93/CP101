b859d98ac19ebca36d55b73a422f7aff878db7ab
ClintJAPHET <ct3418956@gmail.com>
2024-09-20 18:59:21 +0530

    Create Tamala.py

---
+++ Activity 4/Tamala.py
@@ -0,0 +1,29 @@
+# sampleText1 = "My name is {}. I love {}. I love playing {}. {}"
+# sampleText1a = sampleText1.format("Clint", "Steak", "World of Warships", "Orange")
+# print(sampleText1a)
+
+
+
+# sampleText2 = "My name is {1}. I love {2}. I love playing {0}."
+# sampleText2a = sampleText2.format("ML", "Clint", "Steak", "World of Warships", "Orange")
+# print(sampleText2a)
+
+
+
+# sampleText3 = "My name is {name}. I love {food}. I love playing {play}."
+# sampleText3a = sampleText3.format(food="Steak", play="World of Warships", name="Clint")
+# print(sampleText3a)
+
+
+
+
+
+# item = "bluetooth speaker"
+# cost = 200.5
+# Speaker = "The product is %s the cost is %.5f." % (item, cost)
+# print(Speaker) 
+
+item = "Speaker module board"
+cost = 355
+module board = f"The item is {item} and the cost is {cost * 355} pesos."
+print(Module Board)  
