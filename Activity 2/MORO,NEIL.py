64d6406a3bf50df550e04b5025f0053f6950dd17
neilphython <nomandzkie@gmail.com>
2024-09-19 23:18:55 -0700

    Create MORO,NEIL.py

---
+++ Activity 2/MORO,NEIL.py
@@ -0,0 +1,9 @@
+math = float(input("Grade in Math"))
+science = float(input("Grade in Science"))
+english = float(input("Grade in Science"))
+
+sum_of_all_subjects = math + science + english
+
+total_average = sum_of_all_subjects / 3
+
+print(total_average)
