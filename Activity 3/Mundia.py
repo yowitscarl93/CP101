6f74e4d0579d52301e5eabc45a0a68d7884dc104
Geraldmundia <sairaldmundia@gmail.com>
2024-09-24 12:27:39 +0800

    Create Mundia.py

---
+++ Activity 3/Mundia.py
@@ -0,0 +1,6 @@
+# Write a program to prompt the user for hours and rate per hour to compute gross pay.
+
+hour = float(input("Enter number of hours:")) # will ask the user to enter number of hours work.
+rate = float(input("Enter rate:")) # will ask the user to enter the rate per hour.
+grosspay= hour * rate 
+print (grosspay)
