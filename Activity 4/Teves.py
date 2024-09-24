e92e45339b902f330292bd82b449d587d9503e0b
bosstevs <veejayteves714@gmail.com>
2024-09-24 17:06:05 +0800

    Create Teves.py

---
+++ Activity 4/Teves.py
@@ -0,0 +1,31 @@
+
+# sampleText1 = "My name is {}. I love {}. I love playing {}. {}"
+# sampleText1a = sampleText1.format("Vee Jay", "Cup noodles", "Mobile Legends", "Apples")
+# print(sampleText1a)
+
+
+
+# sampleText2 = "My name is {1}. I love {2}. I love playing {0}."
+# sampleText2a = sampleText2.format("COD", "Vee Jay", "Cup noodles", "Mobile Legends", "Apples")
+# print(sampleText2a)
+
+
+
+# sampleText3 = "My name is {name}. I love {food}. I love playing {play}."
+# sampleText3a = sampleText3.format(food="Cup noodles", play="Mobile Legends", name="Vee Jay")
+# print(sampleText3a)
+
+
+
+
+
+# item = "coffee"
+# cost = 20.5
+# SampleText4 = "The product is %s the cost is %.5f." % (item, cost)
+# print(SampleText4) 
+
+item = "Smart watch"
+cost = 2000$
+
+SampleText5 = f"The item is {item} and the cost is {cost * 100} pesos."
+print(SampleText5)  
