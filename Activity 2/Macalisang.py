a6e9b9d8fc004d6e6ebe6deb41316c980b69b5ba
Rey <angelourey1@gmail.com>
2024-09-25 01:03:09 -0700

    Create Macalisang.py

---
+++ Activity 2/Macalisang.py
@@ -0,0 +1,9 @@
+math_subject = float(input("Grade in Math: "))
+
+science_subject = float(input("Grade in Science: "))
+
+english_subject = float(input("Grand in English: "))
+
+sum_of_all_subjects = (math_subject + science_subject + english_subject)/3
+
+print ("The total average grade is: " + str(sum_of_all_subjects))
