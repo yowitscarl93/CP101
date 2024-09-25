3367c16f41dc8faced01c12d4522785697c2ecb1
rodreluya9 <rodalynreluya916@gmail.com>
2024-09-25 20:21:51 +0800

    task1Reluya.py

---
+++ Midterm/TASK 1/Reluya.py
@@ -0,0 +1,7 @@
+tally = float(input("Enter a tally: "))
+if tally > 0:
+    print("The tally is positive.")
+elif tally < 0:
+    print("The tally is negative.")
+else:
+    print("The tally is zero.")
