312ea2aa28b9b9931e212774cef979084a4802e5
Jamaica544 <jamaicatumanao@gmail.com>
2024-09-26 00:32:43 +0800

    Create Tumanao.py

---
+++ Activity 2/Tumanao.py
@@ -0,0 +1,9 @@
+Math = int(input("Enter the first number: "))
+Science = int(input("Enter the second number: "))
+English = int(input("Enter the third number: "))
+
+average = (Math + Science + English) /
+3
+
+print("The avarage of those 3 numbers is:" , average)
+           
