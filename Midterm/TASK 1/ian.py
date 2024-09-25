ec97c12ff8c758df30008695589b51c70aaa207a
Yahooo04 <yahooojauculan@gmail.com>
2024-09-25 23:30:03 +0800

    Create ian.py
    MIDTERM TASK 1

---
+++ Midterm/TASK 1/ian.py
@@ -0,0 +1,8 @@
+number = float(input("Enter a number: "))
+
+if number > 50:
+    print("The number is positive.")
+elif number < 50:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
