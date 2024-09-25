ce9a271253a7a28a73f4fe863f471342fa8df8f2
Chis-tina <christyquino333@gmail.com>
2024-09-25 18:35:41 +0700

    Rusiana.py

---
+++ Activity 2/Rusiana.py
@@ -0,0 +1,5 @@
+math_grade = float(input("Enter Math Grade: "))
+science_grade = float(input("Enter Science Grade: "))
+english_grade = float(input("Enter English Grade: "))
+average_grade = (math_grade + science_grade + english_grade) / 3
+print("Average Grade:", average_grade)
