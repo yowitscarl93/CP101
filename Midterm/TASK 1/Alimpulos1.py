f241d055e633ff8140a852972df6c15525b13c05
Vanz228 <ivanvincentalimpulos@gmail.com>
2024-09-25 22:45:02 +0800

    Create Alimpulos1.py
    Maam nag usob kog pasa maam kay ang una nako na pasa kay na sayop ko. 
    Sorry maam

---
+++ Midterm/TASK 1/Alimpulos1.py
@@ -0,0 +1,8 @@
+digit = float(input("enter number: ")) 
+
+if digit < 0:
+    print("it is a negative number") 
+elif digit > 0:
+    print("it is a positive number")
+else:
+    print("it is a zero")
