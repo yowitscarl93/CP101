074ae52728dd6d80a781f5b45458097643f15530
yowitscarl93 <agustincarlryan93@gmail.com>
2024-09-25 20:38:48 +0800

    Create Agustin.py

---
+++ Midterm/TASK 1/Agustin.py
@@ -0,0 +1,10 @@
+#1.
+
+Number = int(input("Enter a number: "))
+
+if Number > 0:
+    print("The number is positive.")
+elif Number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
