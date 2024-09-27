612978085b5c7a008bd01502dbce012dd3552e91
rodreluya9 <rodalynreluya916@gmail.com>
2024-09-27 08:29:46 +0800

    Reluya.py

---
+++ Activity 3/Reluya.py
@@ -0,0 +1,9 @@
+# Computation for gross pay
+hours = float(input("Enter Hours: "))
+rate_per_hour = float(input("Enter Rate per Hour: "))
+
+# Compute gross pay 
+gross_pay = hours * rate_per_hour
+
+# Print the result
+print("Pay:", gross_pay)
