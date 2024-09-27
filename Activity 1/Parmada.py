f9e9ec618193fc60beff1259dcb2aad8c5785071
faith101242 <maefaith35@gmail.com>
2024-09-27 18:19:46 +0800

    Create Parmada.py

---
+++ Activity 1/Parmada.py
@@ -0,0 +1,18 @@
+#1
+# Find the value of variable x.
+
+x = 5 * 7
+print (x)
+
+#2
+# Find the value of variable y.
+
+y = 3 * x / 4 + 9
+print (y)
+
+#3
+# Find the value of variable z.
+
+z = x + y
+print (z)
+    
