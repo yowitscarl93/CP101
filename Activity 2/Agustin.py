64fea091c3fe1805942ac322a5a2c0383164030c
yowitscarl93 <agustincarlryan93@gmail.com>
2024-09-30 01:19:54 +0800

    Agustin.py

---
+++ Activity 2/Agustin.py
@@ -0,0 +1,7 @@
+Science_gradesheet = float(input("Enter Math grade: "))
+Mathematics_gradesheet = float(input("Enter Science grade: "))
+English_gradesheet = float(input("Enter English grade: "))
+
+average = (Science_gradesheet + Mathematics_gradesheet + English_gradesheet) / 3
+
+print("The student's average is: ", average)
