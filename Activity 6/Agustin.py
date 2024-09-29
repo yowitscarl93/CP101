f32abf42ac8245d4b5c7dda3ae7f5e59e44696e8
yowitscarl93 <agustincarlryan93@gmail.com>
2024-09-30 01:27:41 +0800

    Agustin.py

---
+++ Activity 6/Agustin.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Hello, {name}! You are {age} years old.")
+
+bought = input("enter bought:")
+much = input("how much:")
+message = "i brought {}, {}.".format (bought,much)
+print (message)
+
+city = "Dipolog Pharmacy"
+temperature = 22
+
+print("The temperature in %s is %d degrees Celsius." % (city, temperature))
