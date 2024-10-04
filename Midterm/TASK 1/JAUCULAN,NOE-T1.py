33b1473e2cb12d0fbe8edf5780e559fab3a6617b
Hunya-syete <tinangmarcillones@gmail.com>
2024-10-04 13:44:27 +0800

    pass

--- Midterm/TASK 1/JAUCULAN,NOE-T1.py
+++ Midterm/TASK 1/JAUCULAN,NOE-T1.py
@@ -1,10 +1,10 @@
 int(input("Enter the number"))
 
 if string >10:
-    print("It is positive")
+    print("It is positive")
 
 elif string >0:
-    print("It is negative")
-    
+    print("It is negative")
+
 else:
-    print("The number of zero")
+    print("The number of zero")
